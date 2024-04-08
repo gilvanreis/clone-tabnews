6b979edf2fb5947d99cd8d83d5f4e0d8aa059ed0
gilvanreis <gilvan7reis@gmail.com>
2024-04-08 01:05:17 +0000

    modifica pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1> Letícia Reis, eu amo você, se você me ama dá uma risadinha =D S2</h1>
+    return <h1> Letícia Reis, eu amo você, se você me ama dá uma risadinha :( </h1>
 }
 
 export default Home;
