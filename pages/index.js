962c2cf87eae8fa1d7e533da9cac97b877e75e0e
gilvanreis <gilvan7reis@gmail.com>
2024-04-08 01:32:20 +0000

    refazendo frase da pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1> Letícia Reis, eu amo você, se você me ama dá uma risadinha :( </h1>
+    return <h1> Letícia Reis se me ama dá uma risadinha :D </h1>
 }
 
 export default Home;
