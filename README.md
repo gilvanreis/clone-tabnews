1233f0944a780c44ac984c7f8db27e73992e1843
gilvanreis <gilvan7reis@gmail.com>
2024-04-09 12:28:56 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Implementação do tabnews.com.br para o curso.dev 
+
+Implementação do tabnews.com.br para o curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1> Letícia Reis se me ama dá uma risadinha :D </h1>
+function Home() {
+  return <h1> Letícia Reis se me ama dá uma risadinha :D </h1>;
 }
+function test {
+  let test = "teste";
+
+}
+
 
 export default Home;
