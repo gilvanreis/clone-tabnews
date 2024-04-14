212b16f1fe19235295910bd20313a3c13c2fd824
gilvanreis <gilvan7reis@gmail.com>
2024-04-14 18:23:10 +0000

    alterando style da página em construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-  return <h1> Página em Construção </h1>;
+  return (
+    <div>
+      <h1 style={{ color: "purple" }}>Página em Construção</h1>
+      <p style={{ color: "#B22222" }}>
+        Estamos construindo um novo espaço na internet para contribuir com as
+        suas finanças.
+        <br /> Aguardem novidades!
+      </p>
+    </div>
+  );
 }
-
 export default Home;
