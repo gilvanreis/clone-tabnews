8cd98ee8e3738b71b4e932b96fdf50e5143bed72
gilvanreis <gilvan7reis@gmail.com>
2024-05-01 11:02:31 +0000

    add `database.js´ and `,env` files

--- pages/api/v1/status.js
+++
@@ -1,4 +0,0 @@
-function status(request, response) {
-  response.status(200).json({ chave: "somos acima da média" });
-}
-export default status;
---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,7 @@
+import database from "../../../../infra/database.js";
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response.status(200).json({ chave: "somos acima da média" });
+}
+export default status;
