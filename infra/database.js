e0053a61fd8f1124bf65020449c6edd8329915e6
gilvanreis <gilvan7reis@gmail.com>
2024-05-01 16:35:24 +0000

    ajustes finos

--- infra/database.js
+++ infra/database.js
@@ -2,11 +2,11 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host: process.env.development.POSTGRES_HOST,
-    port: process.env.development.POSTGRES_PORT,
-    user: process.env.development.POSTGRES_USER,
-    database: process.env.development.POSTGRES_DB,
-    password: process.env.development.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
   const result = await client.query(queryObject);
